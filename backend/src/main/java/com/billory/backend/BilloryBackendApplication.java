e3bc59cbc1f4139dccf9f9d83936da7753c263a7
Daniel Schitz <daniel.schitz@web.de>
2025-12-30 17:28:20 +0100

    Setup project structure with Spring Boot and React

---
+++ backend/src/main/java/com/billory/backend/BilloryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.billory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilloryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilloryBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/billory/backend/BilloryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billory.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilloryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
